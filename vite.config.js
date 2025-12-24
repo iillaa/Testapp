43bc441b50bcd0678deb9e05b0924ed78043195c
iillaa <dr.doodo257@gmail.com>
2025-12-24 18:00:44 +0100

    op

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: './',  // <--- CETTE LIGNE EST OBLIGATOIRE POUR ANDROID/CHROME
+    base: './',  // <--- C'EST LA CLÉ POUR L'ÉCRAN BLANC
     })
