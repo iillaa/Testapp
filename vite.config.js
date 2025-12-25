f0dd79f9848c9ff0c0e28e745d8438760d28b976
iillaa <36302274+iillaa@users.noreply.github.com>
2025-12-25 23:15:54 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: './',  // <--- C'EST LA CLÉ POUR L'ÉCRAN BLANC
-    })
+  base: './', // OBLIGATOIRE pour les chemins relatifs (Android/Termux)
+})
